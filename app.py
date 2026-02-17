7b226ff6a60301176c8a47b3cac367d03a19cad4
thallesbobsarmento20-rgb <thalles.bob.sarmento20@gmail.com>
2026-02-17 18:00:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def conectar_planilha():
 
     client = gspread.authorize(creds)
 
-    sheet = client.open("Auditoria_99Food").sheet1
+    sheet = client.open("Auditorias_99Food").sheet1
     return sheet
 
 
